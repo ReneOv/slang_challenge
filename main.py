831f5082128ab9d841ea84d2c4a8559c38c50d14
Rene <a01702860@itesm.mx>
2022-11-27 15:53:02 -0600

    Bug fixing

--- main.py
+++ main.py
@@ -11,11 +11,16 @@ import json
 from datetime import datetime as dt
 
 # Helper functions
-def differenceInSeconds(date1, date2) -> float:
-    parsedDate1: dt.strptime(date1, '%Y-%m-%dT%H:%M:%S.%fZ')
-    parsedDate2: dt.strptime(date2, '%Y-%m-%dT%H:%M:%S.%fZ')
 
-    return (parsedDate2 - parsedDate1).total_seconds()
+def differenceInSeconds(date1, date2) -> float:
+    parsedDate1 = dt.strptime(date1, '%Y-%m-%dT%H:%M:%S.%f+00:00')
+    parsedDate2 = dt.strptime(date2, '%Y-%m-%dT%H:%M:%S.%f+00:00')
+
+    return (parsedDate1 - parsedDate2).total_seconds()
+
+def appendIdArray(array: list, newId) -> list:
+    return array + [newId]
+
 
 # First Step
 # - Fetch API data
@@ -28,8 +33,6 @@ def differenceInSeconds(date1, date2) -> float:
 
 activities_response = json.load(open('response.json'))
 
-print(activities_response['activities'][0])
-
 
 # Second Step
 # - Parse received JSON
@@ -37,12 +40,13 @@ print(activities_response['activities'][0])
 #   - Separate activities into own sessions
 # - Build new data structure
 
-def build_user_sessions(activities_response):               # O(N)
+def build_user_sessions(activities_response) -> any:        # O(N)
     user_sessions = {}
-    for activity in activities_response['activities']:      # O(N)
-        if activity["user_id"] in user_sessions.keys():     # O(1) 
-            if differenceInSeconds(activity["first_seen_at"], user_sessions["user_id"]["ended_at"]) > 300:   # It means that the new activity belongs to a different session.
-                user_sessions["user_id"].append({
+    for activity in activities_response['activities']:      # O(N) - It only iterates through the activity response json once. It mutates the dictionary and updates values every time a new entry is read.
+        if activity["user_id"] in user_sessions.keys():     # O(1) - Dictionaries in Python don't iterate to find keys, since they are hashed.
+            current = len(user_sessions[activity["user_id"]]) - 1
+            if differenceInSeconds(activity["first_seen_at"], user_sessions[activity["user_id"]][current]["ended_at"]) > 300.0:   # It means that the new activity belongs to a different session.
+                user_sessions[activity["user_id"]] = appendIdArray(user_sessions[activity["user_id"]] ,{
                     "started_at": activity["first_seen_at"],
                     "ended_at": activity["answered_at"],
                     "activity_ids": [
@@ -50,17 +54,16 @@ def build_user_sessions(activities_response):               # O(N)
                     ],
                     "duration_seconds": differenceInSeconds(activity["answered_at"], activity["first_seen_at"])
                 })
-            else:                   # Belongs to the most recent session.
-                current = len(user_sessions["user_id"]) - 1                                                                                        
-                user_sessions["user_id"][current] = {
+            else:                   # Belongs to the most recent session.                                                                                   
+                user_sessions[activity["user_id"]][current] = {
+                    "started_at": user_sessions[activity["user_id"]][current]["started_at"],
                     "ended_at": activity["answered_at"],                # Update new end date
-                    "activity_ids": user_sessions["user_id"][current]["activity_ids"].append(activity["id"]),
-                    "duration_seconds": differenceInSeconds(activity["answered_at"], user_sessions["user_id"][current]["started_at"])       # Update session duration
+                    "activity_ids": appendIdArray(user_sessions[activity["user_id"]][current]["activity_ids"], (activity["id"])),
+                    "duration_seconds": differenceInSeconds(activity["answered_at"], user_sessions[activity["user_id"]][current]["started_at"])       # Update session duration
                 }
 
         else:
-            print(activity)
-            user_sessions["user_id"] = [{
+            user_sessions[activity["user_id"]] = [{
                 "started_at": activity["first_seen_at"],
                 "ended_at": activity["answered_at"],
                 "activity_ids": [
@@ -68,6 +71,10 @@ def build_user_sessions(activities_response):               # O(N)
                 ],
                 "duration_seconds": differenceInSeconds(activity["answered_at"], activity["first_seen_at"])
             }]
+    
+    return user_sessions
+    
+    
 
 user_sessions = {"user_sessions": build_user_sessions(activities_response)}
 
