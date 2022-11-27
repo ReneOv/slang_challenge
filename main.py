818544edabe53873a259a205c7a318f697217d3e
Rene <a01702860@itesm.mx>
2022-11-27 13:56:54 -0600

    Copy example code

--- main.py
+++ main.py
@@ -3,12 +3,14 @@
 # Slang Challenge: Engineering Inter
 # #####
 
+# env Variables
+from env import AUTH_TOKEN
 import requests
 
 # First Step
 # - Fetch API data
 activities_response = requests.get("https://api.slangapp.com/challenges/v1/activities",
-                                   headers={"Authorization": "key goes here"})
+                                   headers={"Authorization": AUTH_TOKEN})
 
 
 # Second Step
@@ -17,6 +19,15 @@ activities_response = requests.get("https://api.slangapp.com/challenges/v1/activ
 #   - Separate activities into own sessions
 # - Build new data structure
 
+def build_user_sessions():
+    pass
+
+user_sessions = {"user_sessions": build_user_sessions(activities_response.json())}
+
 
 # Third Step
 # - Post result to endpoint
+
+requests.post("https://api.slangapp.com/challenges/v1/activities/sessions",
+              headers={"Authorization": AUTH_TOKEN},
+              json=user_sessions)
