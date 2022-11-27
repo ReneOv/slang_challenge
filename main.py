178ee04a228ee5a531e2113c78e7da84515f78ad
Rene <a01702860@itesm.mx>
2022-11-27 13:49:01 -0600

    List out steps. Add env and gitignore

--- main.py
+++ main.py
@@ -3,3 +3,20 @@
 # Slang Challenge: Engineering Inter
 # #####
 
+import requests
+
+# First Step
+# - Fetch API data
+activities_response = requests.get("https://api.slangapp.com/challenges/v1/activities",
+                                   headers={"Authorization": "key goes here"})
+
+
+# Second Step
+# - Parse received JSON
+#   - Separate into user
+#   - Separate activities into own sessions
+# - Build new data structure
+
+
+# Third Step
+# - Post result to endpoint
