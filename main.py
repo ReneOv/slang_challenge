5ecbbf31886955734a3a86896eb362a42010760f
Rene <a01702860@itesm.mx>
2022-11-27 14:22:32 -0600

    Create sample response file

--- main.py
+++ main.py
@@ -6,11 +6,16 @@
 # env Variables
 from env import AUTH_TOKEN
 import requests
+import sys
 
 # First Step
 # - Fetch API data
 activities_response = requests.get("https://api.slangapp.com/challenges/v1/activities",
-                                   headers={"Authorization": AUTH_TOKEN})
+                                       headers={"Authorization": AUTH_TOKEN})
+
+# Save to file to not call API every time
+sys.stdout = open('response.txt', 'w')
+print(activities_response.json())
 
 
 # Second Step
@@ -20,14 +25,15 @@ activities_response = requests.get("https://api.slangapp.com/challenges/v1/activ
 # - Build new data structure
 
 def build_user_sessions():
+    
     pass
 
-user_sessions = {"user_sessions": build_user_sessions(activities_response.json())}
+# user_sessions = {"user_sessions": build_user_sessions(activities_response.json())}
 
 
 # Third Step
 # - Post result to endpoint
 
-requests.post("https://api.slangapp.com/challenges/v1/activities/sessions",
-              headers={"Authorization": AUTH_TOKEN},
-              json=user_sessions)
+# requests.post("https://api.slangapp.com/challenges/v1/activities/sessions",
+#             headers={"Authorization": AUTH_TOKEN},
+#             json=user_sessions)
